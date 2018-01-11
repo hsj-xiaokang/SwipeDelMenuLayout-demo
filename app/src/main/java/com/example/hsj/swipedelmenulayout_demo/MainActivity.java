03ee9fb0eca77a29daadf827c5703b2575098055
2356899074@qq.com <2356899074@qq.com>
2018-01-11 13:47:15 +0800

    源码

---
+++ app/src/main/java/com/example/hsj/swipedelmenulayout_demo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hsj.swipedelmenulayout_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * https://github.com/hsj-xiaokang/SwipeDelMenuLayout
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+    }
+}
